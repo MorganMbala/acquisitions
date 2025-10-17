558098f7c658c0270294d626fb632159524a3973
Morgankaiseer <morganmbala@icloud.com>
2025-10-17 19:16:11 -0400

    Implement users CRUD

--- src/app.js
+++ src/app.js
@@ -6,11 +6,9 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
 import securityMiddleware from '#middleware/security.middleware.js';
-import { ne } from 'drizzle-orm';
-
+import usersRoutes from '#routes/users.routes.js';
 
 const app = express();
-
 
 app.use(helmet());
 app.use(cors());
@@ -18,15 +16,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
-  logger.info('Hello from Acquisitions !');
+  logger.info('Hello from Acquisitions!');
 
-  res.status(200).send('Hello from Acquisitions !');
+  res.status(200).send('Hello from Acquisitions!');
 });
 
 app.get('/health', (req, res) => {
@@ -43,7 +44,11 @@ app.get('/api', (req, res) => {
   res.status(200).json({ message: 'Acquisitions API is running!' });
 });
 
-
 app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 
 export default app;
